b06abd684a4b32c10caa062c05e0e543c13859fa
MomoMajesty <momoiphone06@hotnail.fr>
2020-07-25 21:28:45 +0200

    Initial Commit

---
+++ Momo's Pizza/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Momo's Pizza
+//
+//  Created by Momo on 7/25/20.
+//  Copyright © 2020 Momo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
